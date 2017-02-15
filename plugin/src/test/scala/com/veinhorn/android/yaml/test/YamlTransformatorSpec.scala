3ef5181b1f400c8adabb4826bd5f5b795562ea2b
VEINHORN <b.korogvich@gmail.com>
2017-02-15 18:35:57 +0300

    added new test case

--- plugin/src/test/scala/com/veinhorn/android/yaml/test/YamlTransformatorSpec.scala
+++ plugin/src/test/scala/com/veinhorn/android/yaml/test/YamlTransformatorSpec.scala
@@ -16,4 +16,12 @@ class YamlTransformatorSpec extends LayoutSpec {
     (xmlElm \ "@layout_width").text should equal("match_parent")
     (xmlElm \ "@layout_height").text should equal("match_parent")
   }
+
+  it should "test YAML to XML android view transformation (multi ids)" in {
+    val yaml = loadResource("multi_id_view.yaml")
+    val xmlView = new YamlTransformator().transform(yaml)
+
+    // TODO: Implement tags checking
+    val xmlElm = XML.loadString(xmlView)
+  }
 }
